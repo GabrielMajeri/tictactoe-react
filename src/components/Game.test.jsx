ae36bca703c9b1fcf0b52de6428ff2a332c0f52a
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-11-21 08:56:43 +0200

    Add tests for game logic

--- src/components/Game.test.jsx
+++ src/components/Game.test.jsx
@@ -1,8 +1,39 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
 import Game from "./Game";
 
 it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Game />, div);
+  const { getByText } = render(<Game />);
+
+  expect(getByText("Next player: X")).toBeInTheDocument();
+});
+
+function playGame(getAllByRole, moves) {
+  const squares = getAllByRole("button");
+  for (const move of moves) {
+    fireEvent.click(squares[move]);
+  }
+}
+
+async function expectWinner(getByText, winner) {
+  const winnerElem = await waitForElement(() => getByText(`Winner: ${winner}`));
+  expect(winnerElem).toBeInTheDocument();
+}
+
+it("detects when X wins", async () => {
+  const { getAllByRole, getByText } = render(<Game />);
+  playGame(getAllByRole, [0, 1, 4, 2, 8]);
+  await expectWinner(getByText, "X");
+});
+
+it("detects when O wins", async () => {
+  const { getAllByRole, getByText } = render(<Game />);
+  playGame(getAllByRole, [0, 1, 2, 4, 5, 7]);
+  await expectWinner(getByText, "O");
+});
+
+it("detects when it's a draw", async () => {
+  const { getAllByRole, getByText } = render(<Game />);
+  playGame(getAllByRole, [4, 0, 1, 2, 6, 7, 8, 3, 5]);
+  expect(getByText("Draw")).toBeInTheDocument();
 });
